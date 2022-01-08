5282d02504c6567c22a80bc4afe118ad5389609d
Neeffex <neeffex@localhost.localdomain>
2022-01-08 13:46:07 -0500

    nvim

---
+++ nvim/lua/plug-colorizer.lua
@@ -0,0 +1,8 @@
+require'colorizer'.setup(
+    {'*';},
+    {
+    RGB      = true;         -- #RGB hex codes
+    RRGGBB   = true;         -- #RRGGBB hex codes
+    names     = false;
+    mode     = 'background';
+})
