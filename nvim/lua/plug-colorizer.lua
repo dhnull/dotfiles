28b9c2c8adde7df437a696b30a0a8522ce682f76
dhnull <dhnullx@localhost.localdomain>
2022-02-15 19:28:15 -0500

    kitty

--- nvim/lua/plug-colorizer.lua
+++
@@ -1,8 +0,0 @@
-require'colorizer'.setup(
-    {'*';},
-    {
-    RGB      = true;         -- #RGB hex codes
-    RRGGBB   = true;         -- #RRGGBB hex codes
-    names     = false;
-    mode     = 'background';
-})
